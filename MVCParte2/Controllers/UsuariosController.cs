05329c922c0ef47f877dfeb6bc7e7d848670354f
Fernando Estrada <develofer.estrada@gmail.com>
2020-05-07 11:52:00 -0500

    Primer Commit VS

---
+++ MVCParte2/Controllers/UsuariosController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCParte2.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListaUsuarios()
+        {
+            List<string> listaUsuarios = new List<string>();
+            listaUsuarios.Add("Fernando");
+            listaUsuarios.Add("Alejandra");
+            listaUsuarios.Add("Raul");
+            listaUsuarios.Add("Martin");
+            listaUsuarios.Add("Ernesto");
+            return View(listaUsuarios);
+        }
+    }
+}
